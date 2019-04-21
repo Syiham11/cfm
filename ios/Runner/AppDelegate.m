f2106bfa51d9ec50e3473553d50daca0cfa3ec2a
syiham <ihammusoffa@gmail.com>
2019-04-21 14:36:07 +0700

    splash update

--- ios/Runner/AppDelegate.m
+++ ios/Runner/AppDelegate.m
@@ -1,15 +1,11 @@
 #include "AppDelegate.h"
 #include "GeneratedPluginRegistrant.h"
 
-
-// @import UIKit;
-// @import Firebase;
-
 @implementation AppDelegate
-
 
 - (BOOL)application:(UIApplication *)application
     didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
+  UIApplication.sharedApplication.statusBarHidden = false;
   [GeneratedPluginRegistrant registerWithRegistry:self];
   // Override point for customization after application launch.
   return [super application:application didFinishLaunchingWithOptions:launchOptions];
